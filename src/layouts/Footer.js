ad771a8f3808cb37acf9c252c802747c0e5162db
Marcio <mangelino997@gmail.com>
2021-06-04 21:06:30 -0300

    Updates

--- src/layouts/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-    return (
-        <footer className="footer">
-            <span>Meli test - Angelino Marcio</span>
-        </footer>
-    )
-}
