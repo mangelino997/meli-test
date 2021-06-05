210ce7345007d8bd1d767b9294c52d6d8d1df51c
Marcio <mangelino997@gmail.com>
2021-06-04 21:42:08 -0300

    Fix tag Link to Home in Header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -7,7 +7,7 @@ export const Header = () => {
     return (
         <header className="header">
             <div className="nav-skip-to-main-content">
-                <Link to={`${process.env.PATH_PREFIX}/`} />
+                <Link to={`${process.env.PATH_PREFIX}/`} className="nav-logo inline-box"/>
                 <BoxSearch />
             </div>
         </header>
